6da135809bbcfd19d079f7ec76ef280de11ac822
KiyaTilahun <kiyatilahun0@gmail.com>
2023-01-28 16:01:37 +0300

    works well on the messages

---
+++ includes/dbh.inc.php
@@ -0,0 +1,11 @@
+<?php
+
+$dbServerName="localhost";
+$dbUserName="root";
+$dbPassword="";
+$dbName="diary";
+$port = 3306;
+
+$conn  = mysqli_connect($dbServerName,$dbUserName,$dbPassword,$dbName,$port);
+    
+    
